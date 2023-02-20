eed4548c52ca12ed7b5c72b7603ca6b7faca5d2b
OaH <81689054+oah1006@users.noreply.github.com>
2023-02-20 13:34:57 +0700

    Fix: auth.php

--- config/auth.php
+++ config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'staffs',
+        'passwords' => 'staff',
     ],
 
     /*
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'staffs',
+            'provider' => 'staff',
         ],
     ],
 
@@ -60,7 +60,7 @@ return [
     */
 
     'providers' => [
-        'staffs' => [
+        'staff' => [
             'driver' => 'eloquent',
             'model' => App\Models\Staff::class,
         ],
@@ -87,8 +87,8 @@ return [
     */
 
     'passwords' => [
-        'staffs' => [
-            'provider' => 'staffs',
+        'staff' => [
+            'provider' => 'staff',
             'table' => 'password_resets',
             'expire' => 60,
             'throttle' => 60,

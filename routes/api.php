ae34405d1e6b972abe3b265a2a92cb318db739d5
oah1006 <bnhao10062001@gmail.com>
2023-03-22 02:40:58 +0700

    create otp and forgot passowrd route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Attachment\AttachmentController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\Otp\SendOtpController;
+use App\Http\Controllers\Auth\Otp\VerifyOtpController;
 use App\Http\Controllers\Auth\ProfileController;
 use App\Http\Controllers\Customer\CustomerController;
 use App\Http\Controllers\Store\StoreController;
@@ -28,6 +31,9 @@ Route::prefix('private')->name('private.')->group(function() {
     // Auth in admin system
     Route::prefix('auth')->name('auth.')->group(function() {
         Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
+        Route::post('/send-otp', [SendOtpController::class, 'requestOtp'])->name('send-otp');
+        Route::post('/verify-otp', [VerifyOtpController::class, 'verifyOtp'])->name('verify-otp');
+        Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->name('forgot-password');
         Route::post('/logout', [AuthenticationController::class, 'logout'])
             ->name('logout')->middleware('auth:sanctum');
         Route::get('profile', [ProfileController::class, 'showProfile'])

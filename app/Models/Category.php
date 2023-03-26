3ece1959c2e35c807c6b57ef221f5bfc306e2b7d
OaH <81689054+oah1006@users.noreply.github.com>
2023-03-26 10:48:49 +0700

    create categories database and category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,4 +13,8 @@ class Category extends Model
         'name',
         'description'
     ];
+
+    public function product() {
+        return $this->hasMany(Product::class);
+    }
 }
--- database/migrations/2023_03_24_165127_create_categories_table.php
+++ database/migrations/2023_03_24_165127_create_categories_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('description')->nullable();
+            $table->fullText(['name', 'description']);
             $table->timestamps();
         });
     }
